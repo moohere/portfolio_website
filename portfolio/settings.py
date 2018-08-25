7bb0962bdf3047174866c80615e2fc1e52df595f
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-25 19:39:39 -0400

    changes to settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
