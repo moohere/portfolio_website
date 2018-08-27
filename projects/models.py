352215e8f45ba3665e23fbb98920a0d8c337ad8f
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 17:55:52 -0400

    changed fields

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     summary = models.CharField(max_length=200)
-    img = models.ImageField(upload_to='images/')
+    img = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return self.summary
