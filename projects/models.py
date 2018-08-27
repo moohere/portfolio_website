37c010f09724d17db6ca6f636602bb2cea7c56dd
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 19:26:47 -0400

    added default value

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)
     summary = models.CharField(max_length=200)
-    text = models.TextField(max_length=200)
+    text = models.TextField(max_length=200, default="Text")
 
     def __str__(self):
         return self.summary
