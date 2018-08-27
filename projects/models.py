de95c0709e2404b10bc50d2b7a9f542d162d09d8
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 17:45:51 -0400

    added images field

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     summary = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.summary
