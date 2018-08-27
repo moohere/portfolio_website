e16acfc4f9b7763221d2e184c07127377422cd29
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 15:00:32 -0400

    Changes to image dir

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    image = models.ImageField(upload_to='images/ ')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
     def __str__(self):
