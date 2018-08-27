651e468bd2dd6f273151fb30481410b6fc1a799b
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 11:14:00 -0400

    Changed settings for ckeditor

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,7 +134,16 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_IMAGE_BACKEND = "pillow"
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': None,
+    },
+}
 
 try:
      from .local_settings import *
