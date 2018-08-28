9204ac95fccbc8d70a86ef23def4d4b70a9eb6db
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 21:47:17 -0400

    added import

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 from django.views.static import serve
 import projects.views
 
