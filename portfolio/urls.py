d7f5446e81d3da10bf3e886cd9c61bc1e6b0b476
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 21:37:05 -0400

    changes to url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 import projects.views
 
 urlpatterns = [
@@ -10,3 +11,10 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
