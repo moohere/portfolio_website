f8517857dcfeac97dfb1d89ec93cb941c1576cf0
Muher Thalayasingam <muher.singam@gmail.com>
2018-08-27 20:39:32 -0400

    added a few changes to urls

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portfolio',
+        'NAME': 'portfolioDB',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST':'localhost',
